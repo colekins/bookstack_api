55ad4ed5271deb635d06b97d30e0760e909e7118
colekins <colekins@gmail.com>
2017-11-30 11:07:31 -0500

    Removes numericality
    - Removing this validation until can be corrected

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title, :presence => true
   validates :author, :presence => true
-  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  # validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
